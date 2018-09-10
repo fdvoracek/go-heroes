06b94677b6a106452fd131a2983cbfa071539dae
jakub.coufal <jakub.coufal@wandera.com>
2018-09-10 18:20:52 +0200

    Hello server

--- solution/main.go
+++ solution/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type helloServer struct {
+	server http.Server
+}
+
+func (hs *helloServer) start() {
+	go func() {
+		if err := hs.server.ListenAndServe(); err != nil {
+			panic(err)
+		}
+		defer hs.server.Close()
+	}()
+}
 
 func main() {
-	fmt.Printf("Hello world")
+	fmt.Println("Starting server ...")
+
+	hello := &helloServer{
+		server: http.Server{Addr: ":8080"},
+	}
+
+	go hello.start()
+	fmt.Println("Server started")
+
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
 }
